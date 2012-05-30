c00eb56700e1de0bb93b95ded5942d6fbfe3a4be
gugugupan <gugugupan@163.com>
2012-05-30 15:35:14 +0800

    Initial project

---
+++ camera.h
@@ -0,0 +1,7 @@
+#ifndef ___CAMERA___
+#define ___CAMERA___
+
+#include <cv.h>
+#include <cxcore.h>
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "camera.h"
+#include <highgui.h>
+using namespace std;
+
+int main()
+{
+}
